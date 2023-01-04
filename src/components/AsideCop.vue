d22863e2fc1fcc8b6af9b2a46c7fcc000cc1004e
rxc <2585692124@qq.com>
2023-01-04 09:50:17 +0800

    aside组件

---
+++ src/components/AsideCop.vue
@@ -0,0 +1,24 @@
+<template>
+     <el-menu
+      default-active="2">
+     
+      <el-menu-item index="2">
+        <i class="el-icon-menu"></i>
+        <span slot="title">导航二</span>
+      </el-menu-item>
+      <el-menu-item index="3" disabled>
+        <i class="el-icon-document"></i>
+        <span slot="title">导航三</span>
+      </el-menu-item>
+      <el-menu-item index="4">
+        <i class="el-icon-setting"></i>
+        <span slot="title">导航四</span>
+      </el-menu-item>
+    </el-menu>
+</template>
+
+<script>
+export default {
+    name : 'AsideCop'
+}
+</script>
