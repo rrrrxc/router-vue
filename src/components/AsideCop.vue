9d337bd1ca7d85c10f4ef5cdfc5517eec676731c
rxc <2585692124@qq.com>
2023-01-04 14:51:47 +0800

    主站完善

--- src/components/AsideCop.vue
+++ src/components/AsideCop.vue
@@ -1,29 +1,35 @@
 <template>
-     <el-menu :default-active="activeFlag" @select="active">
-      
-      <el-menu-item index="2">
+  <div class="aside-container">
+    <el-menu :default-active="activeFlag" @select="active">
+      <el-menu-item index="0">
         <i class="el-icon-menu"></i>
-        <span slot="title">导航二</span>
+        <span slot="title">首页</span>
       </el-menu-item>
-      <el-menu-item index="3" >
+      <el-menu-item index="1" >
         <i class="el-icon-document"></i>
-        <span slot="title">导航三</span>
+        <span slot="title">数据库</span>
       </el-menu-item>
-      <el-menu-item index="4">
-        <i class="el-icon-setting"></i>
-        <span slot="title">导航四</span>
-      </el-menu-item>
+      <!-- <div style="height:60px"></div> -->
     </el-menu>
+   
+
+     <div class="aside-footer">
+      @Copyright 2023
+     </div>
+  </div>
+
+ 
 </template>
 
 <script>
 const routeMap = new Map()
-routeMap.set('2','/main/default')
+routeMap.set('0','/main/default')
+routeMap.set('1','/main/data')
 export default {
     name : 'AsideCop',
     data:function () {
       return {
-        activeFlag:''
+        activeFlag:'0'
       }
     },
     methods : {
@@ -31,9 +37,38 @@ export default {
         this.activeFlag=index;
         if(routeMap.has(index))
         {
-          this.$router.push(routeMap.get(index))
+          //防止目标url和当前url相同，push会报错
+          const aimUrl=routeMap.get(index);
+          const currentUrl= this.$router.currentRoute.fullPath
+          if(aimUrl !== currentUrl)
+          {
+            this.$router.push(routeMap.get(index))
+          }
         }
       }
     }
 }
 </script>
+<style lang="scss" scoped>
+.aside-container {
+  height :calc(100% - 60px);
+  ul {
+      height:100%;
+      overflow-y:auto;
+      padding-bottom: 60px;
+    }
+}
+.aside-footer {
+  position:fixed;
+  width:264px;
+  height:60px;
+  left:0;
+  bottom:0;
+  background-color:aqua;
+  border-top: 1px solid #e6e6e6;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 12px;
+}
+</style>
